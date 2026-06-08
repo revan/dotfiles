6f53242b75a2f95a8c20d9db69ec200adbe3f638
Revan Sopher <rsopher@gmail.com>
2026-06-08 00:34:20 -0400

    misc fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,20 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-source /opt/homebrew/opt/asdf/libexec/asdf.fish
 set -x GPG_TTY (tty)
+
+export PATH="$HOME/.local/bin:$PATH"
+
+# ASDF configuration code
+if test -z $ASDF_DATA_DIR
+    set _asdf_shims "$HOME/.asdf/shims"
+else
+    set _asdf_shims "$ASDF_DATA_DIR/shims"
+end
+
+# Do not use fish_add_path (added in Fish 3.2) because it
+# potentially changes the order of items in PATH
+if not contains $_asdf_shims $PATH
+    set -gx --prepend PATH $_asdf_shims
+end
+set --erase _asdf_shims
